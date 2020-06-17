2229814038abd0e9de61276ca2e6063efc6ef703
shangk669 <1181860777@qq.com>
2020-06-17 17:40:38 +0800

    Initial commit

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class HelloWorld {
+    //main的快捷方式  psvm
+    public static void main(String[] args) {
+        //输出语句的快捷方式 sout
+        System.out.println("hello world");
+    }
+}
---
+++ src/day01/TestJunit.java
@@ -0,0 +1,15 @@
+package day01;
+
+import org.junit.Test;
+
+public class TestJunit {
+    @Test
+    public void sayHello(){
+        System.out.println("hello");
+    }
+    @Test
+    public void sayHello1(){
+        System.out.println("hello1");
+    }
+}
+
